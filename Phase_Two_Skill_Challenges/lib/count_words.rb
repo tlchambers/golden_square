ff0e77c8937a7427cb730a5a0c9cd09a1b4e6d36
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2022-12-12 15:33:41 +0000

    count_words challenge

---
+++ Phase_Two_Skill_Challenges/lib/count_words.rb
@@ -0,0 +1,31 @@
+# Design
+# A method called count_words that takes a string as an argument and returns the number of words in that string.
+
+# Method Design Recipe
+
+# {{PROBLEM}} Method Design Recipe
+
+# 1. Describe the Problem
+# A method called count_words 
+# That takes a string as an argument and returns the number of words in that string.
+
+
+# 2. Design the Method Signature
+# Include the name of the method, its parameters, return value, and side effects.
+# EXAMPLE
+def count_words(string)
+  return integer
+end
+
+
+# 3. Create Examples as Tests
+# Make a list of examples of what the method will take and return.
+# EXAMPLE
+
+# count_words("The fox jumps etc.").to eq 4
+# count_words("The fox jumps etc., The fox jumps etc.").to eq 8
+# count_words("") fails "no string given"
+
+
+# 4. Implement the Behaviour
+# After each test you write, follow the test-driving process of red, green, refactor to implement the behaviour.
