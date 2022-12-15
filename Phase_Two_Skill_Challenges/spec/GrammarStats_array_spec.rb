f7a620a7392086991f23b0ea93250e90b54a08ab
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2022-12-15 20:22:33 +0000

    GrammarStats Exercise Updated File

--- Phase_Two_Skill_Challenges/spec/GrammarStats_array_spec.rb
+++ Phase_Two_Skill_Challenges/spec/GrammarStats_array_spec.rb
@@ -1,48 +1,48 @@
 require 'GrammarStats_array'
 
 RSpec.describe GrammarStats_array do
-  # it "fails" do
-  #   sentence = GrammarStats_array.new
-  #   expect{sentence.check("")}. to raise_error "No text provided!"
-  # end
+  it "fails" do
+    sentence = GrammarStats_array.new
+    expect{sentence.check("")}. to raise_error "No text provided!"
+  end
 
-  # it 'text to returns false if text does not start with a capital letter' do
-  #   sentence = GrammarStats_array.new
-  #   result = sentence.check("hello world")
-  #   expect(result).to eq false
-  # end
+  it 'text to returns false if text does not start with a capital letter' do
+    sentence = GrammarStats_array.new
+    result = sentence.check("hello world")
+    expect(result).to eq false
+  end
 
-  # it 'checks if text starts with a capital letter and ends with !' do
-  #   sentence = GrammarStats_array.new
-  #   result = sentence.check("Hello world!")
-  #   expect(result).to eq true
-  # end
+  it 'checks if text starts with a capital letter and ends with !' do
+    sentence = GrammarStats_array.new
+    result = sentence.check("Hello world!")
+    expect(result).to eq true
+  end
 
-  # it 'checks if text starts with a capital letter and ends with ?' do
-  #   sentence = GrammarStats_array.new
-  #   result = sentence.check("Hello world? How are you today?")
-  #   expect(result).to eq true
-  # end
+  it 'checks if text starts with a capital letter and ends with ?' do
+    sentence = GrammarStats_array.new
+    result = sentence.check("Hello world? How are you today?")
+    expect(result).to eq true
+  end
   
-  # it 'checks if text starts with a capital letter and ends with .' do
-  #   sentence = GrammarStats_array.new
-  #   result = sentence.check("Hello world? How are you today.")
-  #   expect(result).to eq true
-  # end
+  it 'checks if text starts with a capital letter and ends with .' do
+    sentence = GrammarStats_array.new
+    result = sentence.check("Hello world? How are you today.")
+    expect(result).to eq true
+  end
   
-  # it 'checks when text starts with a  capital letter but does not ends with appropriate sentence ending' do
-  #   sentence = GrammarStats_array.new
-  #   result = sentence.check("Hello world? How are you today")
-  #   expect(result).to eq false
-  # end
+  it 'checks when text starts with a  capital letter but does not ends with appropriate sentence ending' do
+    sentence = GrammarStats_array.new
+    result = sentence.check("Hello world? How are you today")
+    expect(result).to eq false
+  end
 
-  # context "if one string checked" do
-  #   it "shoud return 100%" do
-  #     sentence = GrammarStats_array.new
-  #     sentence.check("One two three.")
-  #     expect(sentence.percentage_good).to eq "100%"
-  #   end
-  # end
+  context "if one string checked" do
+    it "shoud return 100%" do
+      sentence = GrammarStats_array.new
+      sentence.check("One two three.")
+      expect(sentence.percentage_good).to eq "100%"
+    end
+  end
 
   context "if two strings checked snd one is invalid" do
     it "should return 50%" do
@@ -53,14 +53,14 @@ RSpec.describe GrammarStats_array do
     end
   end
 
-  # context "if three strings checked and one is invalid" do
-  #   it "should return 67%" do
-  #     sentence = GrammarStats_array.new
-  #     sentence.check("One two three.")
-  #     sentence.check("One two three")
-  #     sentence.check("One two three!")
-  #     expect(sentence.percentage_good).to eq "67%"
-  #   end
-  # end
+  context "if three strings checked and one is invalid" do
+    it "should return 67%" do
+      sentence = GrammarStats_array.new
+      sentence.check("One two three.")
+      sentence.check("One two three")
+      sentence.check("One two three!")
+      expect(sentence.percentage_good).to eq "67%"
+    end
+  end
 end
 
